20f116eeded01fabc64e7fcc73879d24a5410457
vantablanta <njerimwanjiku@gmail.com>
2021-05-17 12:52:54 +0300

    fundamentals

---
+++ file1.py
@@ -0,0 +1,34 @@
+print ("Hello World!")
+#This is a comment
+print ('Hi Michelle')
+#this is a second comment 
+print (2021 - 1996)
+print("hello" + "john")
+#multiline strings
+print("""
+First Line 
+Second Line 
+Third Line 
+""")
+#escaped characters
+print("hello \n World")#newline
+print("hello \t world")#tab
+print ("hello \\ world")#to show the backlash as a string
+
+print ("Hello Michelle")
+
+print ("There are two types os slashes: \\ and /")
+
+print ("\n\t This is a tabbed line \n This is a newline")
+print ("""
+(\\(\\
+(-.-)
+o_(\") (\")
+""")
+
+print("""
+Roses are red 
+Violets are \\blue/
+I love \\\\Python//
+And I hope you do too!\U0001F601
+""")
