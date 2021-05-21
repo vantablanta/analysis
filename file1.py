b234b2bc8583b9b67128d87e208488dc89ee9b33
vantablanta <njerimwanjiku@gmail.com>
2021-05-21 17:34:58 +0300

    University of waterloo

---
+++ file1.py
@@ -0,0 +1,4 @@
+#undesrstanding errors 
+#1. synatx error  - occurs because of the wrong syntax pyhton cant understand what you are saying 
+#2. runtime error -error that ocurs after the code is run 
+#. logic error - the program runs well but doesnt excute what you want it to
